2386445b77ec09f901f79795cc51d9e03378518c
Thanh Ly <thanhgly.se@gmail.com>
2023-04-27 17:25:40 -0700

    test request pulling

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,5 +5,5 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
-  expect('aaa').toBe('bbb');
+  expect('aaa').toBe('222');
 });
