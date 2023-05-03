a63cc8e580ffdd852d5bd25023622ff5e0f4839b
Gabriela Taylor <gabriela@Gabrielas-iMac-16.lan>
2023-05-03 10:13:56 -0400

    Updated front-end boilerplate for Take Quiz Feature

--- client/src/components/take-quiz/Quiz.jsx
+++ client/src/components/take-quiz/Quiz.jsx
@@ -2,7 +2,11 @@ import './styles/take-quiz.css';
 import Timer from './quiz-components/Timer.jsx';
 import Question from './quiz-components/Question.jsx';
 import Answers from './quiz-components/Answers.jsx';
+<<<<<<< Updated upstream
 import { Typography, Button } from '@mui/material';
+=======
+import { Typography } from '@mui/material';
+>>>>>>> Stashed changes
 
 function Quiz() {
   return (
@@ -12,8 +16,13 @@ function Quiz() {
       <Answers />
       <Typography>2/25 Questions</Typography>
       
+<<<<<<< Updated upstream
       <Button>&lt;</Button>
       <Button>&gt;</Button>
+=======
+      <>&lt;</>
+      <>&gt;</>
+>>>>>>> Stashed changes
     </>
   );
 }
--- client/src/components/take-quiz/quiz-components/Summary.jsx
+++ client/src/components/take-quiz/quiz-components/Summary.jsx
@@ -1,12 +1,14 @@
 import '../styles/take-quiz.css';
-import { Container, Typography, Button } from '@mui/material';
+import { CssBaseline, Typography, Button } from '@mui/material';
+import Review from './Review.jsx';
 
 function Summary() {
   return (
     <>
         <Typography variant="h6">Conditional Message</Typography>
         <Typography variant="h6">Congrats! if you scored &gt;70% or Aw, Shucks You Should Probably Study</Typography>
-        <Container variant="h4">Review Your Answers</Container>
+        <CssBaseline></CssBaseline>
+        <Review />
         <Button variant="h4">Retake Quiz</Button>
         <Button variant="h4">More Quizzes</Button>
     </>
