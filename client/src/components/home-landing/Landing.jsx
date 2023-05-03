f65be78e65abccc22f1e70ada9ee53e352c6d425
San Chui <chuisan3021@gmail.com>
2023-05-03 16:57:05 -0700

    Landing skeleton

--- client/src/components/home-landing/Landing.jsx
+++ client/src/components/home-landing/Landing.jsx
@@ -1,11 +1,11 @@
-import './styles/home.css';
+import './styles/landing.css';
 import LoginSignin from './components/LoginSignin.jsx';
 import NavigationBar from './components/NavigationBar.jsx';
 import QuizList from './components/QuizList.jsx';
 import SearchBar from './components/SearchBar.jsx';
 import { useState, useEffect } from 'react';
 
-const Home = (props) => {
+const Landing = (props) => {
 
   return (
     <>
@@ -17,4 +17,4 @@ const Home = (props) => {
   );
 };
 
-export default Home;
+export default Landing;
