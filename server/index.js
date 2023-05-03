d2ddec8864359a8be072c48727846d37cf6b89f1
Gabriela Taylor <gabriela@Gabrielas-iMac-17.lan>
2023-05-03 15:27:13 -0400

    Added axios and path as a server dev deps, re-run rpm run install from root after pulling down. Added backend test route to server/index.js for confirming server works on start-up

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 8080;
-const path = require('path');
+const axios = require('axios');
+const port = process.env.SERVER_PORT || 8080;
 
 // =============================================
 //                Middleware
@@ -12,7 +12,11 @@ app.use(express.json());
 // =============================================
 //               Route Imports
 // =============================================
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/api', (req, res) => {
+  res.json('Hello Quiz Whiz Backend')  
+});
+
+app.use('/quiz', require('./routes/take-quiz'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
