14b2f27f4820cb3e60ea241710d9654707ee3e32
Gabriela Taylor <gabriela@Gabrielas-iMac-22.lan>
2023-05-04 15:44:03 -0400

    Removed test Links from App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import "./styles/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Typography } from "@mui/material";
 import Nav from "./Nav.jsx";
 import TakeQuiz from "./components/take-quiz/TakeQuiz.jsx";
@@ -18,10 +17,6 @@ function App() {
           element={
             <div className="body">
               <Typography variant="h4">Welcome to Quiz Whiz</Typography>
-              {/* Using this for Take Quiz Development. Please leave here for now  */}
-              {/* <Link to="/quiz/1/start">Take Quiz</Link>
-              <Link to="/quiz/1/question">Quiz Question</Link>
-              <Link to="/quiz/1/summary">Quiz Summary</Link> */}
             </div>
           }
         ></Route>
