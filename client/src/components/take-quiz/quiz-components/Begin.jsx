aa993ddcb9fd441e324410afc7742608ef6ef3a9
Gabriela Taylor <gabriela@Gabrielas-iMac-21.lan>
2023-05-04 10:44:40 -0400

    Modified the Material UI Theme Palette to use our Application colors instead of the Material UI default palette, wrapped App in top-level index.js with Theme Provider and added some example usage to the Take Quiz Begin screen

--- client/src/components/take-quiz/quiz-components/Begin.jsx
+++ client/src/components/take-quiz/quiz-components/Begin.jsx
@@ -1,9 +1,22 @@
-import '../styles/take-quiz.css';
-import { Typography } from '@mui/material';
+import "../styles/take-quiz.css";
+import { Typography, Button } from "@mui/material";
 
 function Begin() {
   return (
-    <Typography variant="h4">Begin The Quiz?</Typography>
+    <>
+      <Typography>Ready to Start?</Typography>
+      <Typography>Set Timer Option Here?</Typography>
+      <hr />
+      <Typography>FROM DATABASE: Quiz Category here</Typography>
+      <Typography>FROM DATABASE: Quiz Difficulty here</Typography>
+
+      <Button variant="contained" color="primary">
+        Begin Quiz
+      </Button>
+      <Button variant="contained" color="secondary">
+        Back to Browse
+      </Button>
+    </>
   );
 }
 
