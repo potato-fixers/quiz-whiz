ac2cb3c54093f016d1ea916f8d0e70da59f96758
Tiger Hong <tigerhong186@gmail.com>
2023-05-03 20:33:05 -0700

    Dynamic Multiple Choice Question Form created.
    Using react useState, I was able to create a dynamic form field. This allows the user to add fields for new questions as they would like while also retaining the user information in each field even if new fields are added. The option is limited to client request at a maximum of 20 questions and will be refactored to include TF questions in the next commit. The total will be calculated via both TF and MC # of fields, a pop up message will occur if the user attempts to go beyond the set maximum.

--- client/src/components/create-quiz/CreateQuiz.jsx
+++ client/src/components/create-quiz/CreateQuiz.jsx
@@ -10,10 +10,29 @@ import APIQuestions from './quiz-components/API-Questions.jsx';
 // state will be passed down to both components along with update functions.
 
 const CreateQuiz = (props) => {
+
+    // create useState form fields here
+    const [MCInputFields, setMCInputFields] = useState([{question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}, {question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}, {question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}, {question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}, {question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}])
+
+    const handleFormChange = (e, index) => {
+      var data = [...MCInputFields]
+      data[index][e.target.name] = e.target.value
+      console.log(MCInputFields);
+    }
+
+    const addMCFields = () => {
+      var newField = {question: '', corrAns: '', incAns1: '', incAns2: '', incAns3: ''}
+      if (MCInputFields.length < 20) {
+        setMCInputFields([...MCInputFields, newField]);
+      } else {
+        alert('Maximum Questions Reached!')
+      }
+    }
+
   return (
     <div className='createQuiz'>
       <Categories />
-      <MCQuestions />
+      <MCQuestions inputFields={MCInputFields} setInputFields={setMCInputFields} handleFormChange={handleFormChange} addFields={addMCFields}/>
       <TFQuestions />
       <APIQuestions />
       <button> Create Quiz ! </button>
--- client/src/components/create-quiz/quiz-components/MC-Questions.jsx
+++ client/src/components/create-quiz/quiz-components/MC-Questions.jsx
@@ -1,9 +1,24 @@
 import '../styles/create-quiz.css';
+import { useState, useEffect } from 'react';
 
 const MCQuestions = (props) => {
+
   return (
     <div className='MCQuestions'>
-      Multiple Choice Questions go here
+      <form>
+        {props.inputFields.map((input, index) => {
+          return (
+            <div key={index}>
+              <textarea name='question' rows={4} cols={40} placeholder='Type Question Here' onChange={(e) => {props.handleFormChange(e, index)}}></textarea>
+              <input name='corrAns' placeholder='Correct Answer' onChange={(e) => {props.handleFormChange(e, index)}}></input>
+              <input name='incAns1' placeholder='Incorrect Answer' onChange={(e) => {props.handleFormChange(e, index)}}></input>
+              <input name='incAns2' placeholder='Incorrect Answer' onChange={(e) => {props.handleFormChange(e, index)}}></input>
+              <input name='incAns3' placeholder='Incorrect Answer' onChange={(e) => {props.handleFormChange(e, index)}}></input>
+            </div>
+          )
+        })}
+      </form>
+      <button onClick={props.addFields}> Add More Questions! </button>
     </div>
   );
 }
