6f49b85b72bba55cc9f58e3f967f32a5b0b7dc12
Gabriela Taylor <gabriela@Gabrielas-iMac-21.lan>
2023-05-04 11:58:12 -0400

    Patched refresh page redirects to home screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,37 @@
-import './styles/App.css';
+import "./styles/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Typography } from '@mui/material';
-import Nav from './Nav.jsx';
-import TakeQuiz from './components/take-quiz/TakeQuiz.jsx';
-import Dashboard from './components/user-dashboard/Dashboard.jsx';
-import Login from '../src/pages/Login';
-import Register from '../src/pages/Register';
+import { Typography } from "@mui/material";
+import Nav from "./Nav.jsx";
+import TakeQuiz from "./components/take-quiz/TakeQuiz.jsx";
+import Dashboard from "./components/user-dashboard/Dashboard.jsx";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
 
 function App() {
   return (
     <Router>
       <Nav />
       <Routes>
-          <Route path='/*' element={
-            <div className='body'>
-                <Typography variant="h4">Welcome to Quiz Whiz</Typography>
+        <Route
+          path="/*"
+          element={
+            <div className="body">
+              <Typography variant="h4">Welcome to Quiz Whiz</Typography>
+              {/* Using this for Take Quiz Development. Please leave here for now  */}
+              {/* <Link to="/quiz/1/start">Take Quiz</Link>
+                <Link to="/quiz/1/question">Quiz Question</Link>
+                <Link to="/quiz/1/summary">Quiz Summary</Link> */}
             </div>
-          }>
-          </Route>
+          }
+        ></Route>
 
-          <Route path='/quiz/:id/*' element={<TakeQuiz />}></Route>
+        <Route path="/quiz/:id/*" element={<TakeQuiz />}></Route>
 
-          <Route path='dashboard/*' element={<Dashboard />}></Route>
+        <Route path="dashboard/*" element={<Dashboard />}></Route>
 
-          <Route path='/login/' element={<Login />}></Route>
-          <Route path='/register/' element={<Register />}></Route>
-
-        </Routes>
+        <Route path="/login/" element={<Login />}></Route>
+        <Route path="/register/" element={<Register />}></Route>
+      </Routes>
     </Router>
   );
 }
