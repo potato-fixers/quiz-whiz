ac1437038eada10fb755d6a03677d3fa2aec8d54
bennyv8 <bvanhr@gmail.com>
2023-05-06 12:40:54 -0700

    refactored database/index.js

--- server/database/index.js
+++ server/database/index.js
@@ -1,17 +1,1 @@
-require('dotenv').config();
-const db = require('./config/index');
-
-//               DB Methods               //
-let createQuiz = (quizData, calback) => {
-  // db connection created, insert statement depending on what the data looks like
-  db.query('insert statement', (err, res) => {
-    if (err) {
-      console.log(err);
-      callback(err, null)
-    } else {
-      callback(null, res)
-    }
-  })
-}
-
-module.exports.createQuiz = createQuiz;
+module.exports = require('./config/index');
