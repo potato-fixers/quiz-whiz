eba0778a2264666cd3988d7e78ee3b6651a05d29
San Chui <chuisan3021@gmail.com>
2023-05-08 15:55:22 -0700

    Landing page functional implement

--- client/src/components/home-landing/Landing.jsx
+++ client/src/components/home-landing/Landing.jsx
@@ -1,19 +1,26 @@
-import './styles/landing.css';
-import LoginSignin from './components/LoginSignin.jsx';
-import NavigationBar from './components/NavigationBar.jsx';
+import './styles/home.css';
+import CategoryList from './components/CategoryList.jsx';
 import QuizList from './components/QuizList.jsx';
-import SearchBar from './components/SearchBar.jsx';
+import freeQuiz from './mock_data/freeQuiz.js'
+
 import { useState, useEffect } from 'react';
 
 const Landing = (props) => {
 
+  const [quizzes, setQuizzes] = useState([]);
+  const [category, setCategory] = useState("General Knowledge");
+
+  useEffect(() => {
+    setQuizzes(freeQuiz);
+  }, []);
+
   return (
-    <>
-      <NavigationBar />
-      <LoginSignin />
-      <SearchBar />
-      <QuizList />
-    </>
+    <div className="Landing">
+      <h1> Welcome to Quiz-Whiz </h1>
+      <CategoryList setCategory={setCategory}/>
+      <p> Login to see all Quizzes</p>
+      <QuizList category={category} quizzes={quizzes}/>
+    </div>
   );
 };
 
