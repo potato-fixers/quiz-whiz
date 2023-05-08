ae6821d420efa409b0dbef568357fafb013c18f4
Gabriela Taylor <gabriela@Gabrielas-iMac-39.lan>
2023-05-08 17:06:26 -0400

    Moved Modal styles to take-quiz stylesheet

--- client/src/components/take-quiz/Modal.jsx
+++ client/src/components/take-quiz/Modal.jsx
@@ -4,20 +4,6 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  // width: 400,
-  bgcolor: "var(--white)",
-  border: "2px solid var(--yellow)",
-  boxShadow: 24,
-  p: 4,
-  color: "var(--charcoal)",
-  borderRadius: "15px",
-};
-
 export default function BasicModal({ message }) {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
@@ -46,7 +32,7 @@ export default function BasicModal({ message }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box id="take-quiz-modal">
           <Typography id="modal-modal-title" variant="h6" component="h2">
             Are you sure you want to abandon your progress? Your score will not
             be saved.
