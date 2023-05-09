dddb758aca691888042f79888e6c4e71fe68e66a
bennyv8 <bvanhr@gmail.com>
2023-05-08 18:58:21 -0700

    hotfix import error

--- server/routes/user-auth-route.js
+++ server/routes/user-auth-route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/user-auth');
+const controller = require('../controllers/authentication/user-auth');
 
 
 router.post('/login', (req, res) => {
