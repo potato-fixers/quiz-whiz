027a69e810f6729b512ba907d952524339b8e51d
Tiger Hong <tigerhong186@gmail.com>
2023-05-08 18:56:13 -0700

    Cors hotfix.

--- server/index.js
+++ server/index.js
@@ -14,9 +14,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors(
-  origin: 'http://localhost:3000'
-))
+app.use(cors({origin: 'http://localhost:3000'}))
 
 // =============================================
 //               Route Imports
