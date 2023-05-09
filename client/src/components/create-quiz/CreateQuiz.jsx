3b91a25f7866b7055073be201e7c86dc1e2c2089
Tiger Hong <tigerhong186@gmail.com>
2023-05-08 18:36:02 -0700

    Synatx errors resolved

--- client/src/components/create-quiz/CreateQuiz.jsx
+++ client/src/components/create-quiz/CreateQuiz.jsx
@@ -9,68 +9,69 @@ import APIQuestions from "./quiz-components/API-Questions.jsx";
 // state will be passed down to both components along with update functions.
 
 const CreateQuiz = (props) => {
-  // useStates here
-  const [quizName, setQuizName] = useState("");
-  const [TFValidation, setTFValidation] = useState(false);
-  const [MCValidation, setMCValidation] = useState(false);
-  const [MCInputFields, setMCInputFields] = useState([
-    { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
-    { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
-    { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
-  ]);
-  const [TFInputFields, setTFInputFields] = useState([
-    { question: "", corrAns: "", incAns: "" },
-    { question: "", corrAns: "", incAns: "" },
-  ]);
-  // create state here to update which category is the selected
-  const [category1, setState1] = useState(false);
-  const [category2, setState2] = useState(false);
-  const [category3, setState3] = useState(false);
-  const [category4, setState4] = useState(false);
-  const [category5, setState5] = useState(false);
-  const [categoryVal, setCategoryVal] = useState(null);
 
-  // useState function
-  const onSelect = (e) => {
-    e.preventDefault();
+   // useStates here
+   const [quizName, setQuizName] = useState("");
+   const [TFValidation, setTFValidation] = useState(false);
+   const [MCValidation, setMCValidation] = useState(false);
+   const [MCInputFields, setMCInputFields] = useState([
+     { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
+     { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
+     { question: "", corrAns: "", incAns1: "", incAns2: "", incAns3: "" },
+   ]);
+   const [TFInputFields, setTFInputFields] = useState([
+     { question: "", corrAns: "", incAns: "" },
+     { question: "", corrAns: "", incAns: "" },
+   ]);
+   // create state here to update which category is the selected
+   const [category1, setState1] = useState(false);
+   const [category2, setState2] = useState(false);
+   const [category3, setState3] = useState(false);
+   const [category4, setState4] = useState(false);
+   const [category5, setState5] = useState(false);
+   const [categoryVal, setCategoryVal] = useState(null);
 
-    if (e.target.name === "category1") {
-      setState1(true);
-      setState2(false);
-      setState3(false);
-      setState4(false);
-      setState5(false);
-      setCategoryVal(e.target.name);
-    } else if (e.target.name === "category2") {
-      setState1(false);
-      setState2(true);
-      setState3(false);
-      setState4(false);
-      setState5(false);
-      setCategoryVal(e.target.name);
-    } else if (e.target.name === "category3") {
-      setState1(false);
-      setState2(false);
-      setState3(true);
-      setState4(false);
-      setState5(false);
-      setCategoryVal(e.target.name);
-    } else if (e.target.name === "category4") {
-      setState1(false);
-      setState2(false);
-      setState3(false);
-      setState4(true);
-      setState5(false);
-      setCategoryVal(e.target.name)
-    } else {
-      setState1(false);
-      setState2(false);
-      setState3(false);
-      setState4(false);
-      setState5(true);
-      setCategoryVal(e.target.name)
-    }
-  };
+   // useState function
+   const onSelect = (e) => {
+     e.preventDefault();
+
+     if (e.target.name === "category1") {
+       setState1(true);
+       setState2(false);
+       setState3(false);
+       setState4(false);
+       setState5(false);
+       setCategoryVal(e.target.name);
+     } else if (e.target.name === "category2") {
+       setState1(false);
+       setState2(true);
+       setState3(false);
+       setState4(false);
+       setState5(false);
+       setCategoryVal(e.target.name);
+     } else if (e.target.name === "category3") {
+       setState1(false);
+       setState2(false);
+       setState3(true);
+       setState4(false);
+       setState5(false);
+       setCategoryVal(e.target.name);
+     } else if (e.target.name === "category4") {
+       setState1(false);
+       setState2(false);
+       setState3(false);
+       setState4(true);
+       setState5(false);
+       setCategoryVal(e.target.name)
+     } else {
+       setState1(false);
+       setState2(false);
+       setState3(false);
+       setState4(false);
+       setState5(true);
+       setCategoryVal(e.target.name)
+     }
+   };
 
     const handleFormChange = (e, index) => {
       e.preventDefault();
@@ -94,41 +95,40 @@ const CreateQuiz = (props) => {
         }
       }
     }
-  };
 
-  const nameChange = (e) => {
-    e.preventDefault();
-    setQuizName(e.target.value);
-  };
+   const nameChange = (e) => {
+     e.preventDefault();
+     setQuizName(e.target.value);
+   };
 
-  // button functionality here
+   // button functionality here
 
-  const addFields = (e) => {
-    e.preventDefault();
-    var newMCField = {
-      question: "",
-      corrAns: "",
-      incAns1: "",
-      incAns2: "",
-      incAns3: "",
+    const addFields = (e) => {
+      e.preventDefault();
+      var newMCField = {
+        question: "",
+        corrAns: "",
+        incAns1: "",
+        incAns2: "",
+        incAns3: "",
+      };
+      var newTField = { question: "", corrAns: "", incAns: "" };
+      if (
+        MCInputFields.length + TFInputFields.length < 20 &&
+        e.target.name === "MCButton"
+      ) {
+        setMCInputFields([...MCInputFields, newMCField]);
+        setMCValidation(false);
+      } else if (
+        TFInputFields.length + MCInputFields.length < 20 &&
+        e.target.name === "TFButton"
+      ) {
+        setTFInputFields([...TFInputFields, newTField]);
+        setTFValidation(false);
+      } else {
+        alert("Maximum Questions Reached!");
+      }
     };
-    var newTField = { question: "", corrAns: "", incAns: "" };
-    if (
-      MCInputFields.length + TFInputFields.length < 20 &&
-      e.target.name === "MCButton"
-    ) {
-      setMCInputFields([...MCInputFields, newMCField]);
-      setMCValidation(false);
-    } else if (
-      TFInputFields.length + MCInputFields.length < 20 &&
-      e.target.name === "TFButton"
-    ) {
-      setTFInputFields([...TFInputFields, newTField]);
-      setTFValidation(false);
-    } else {
-      alert("Maximum Questions Reached!");
-    }
-  };
 
     const removeFields = (e, index) => {
       e.preventDefault();
@@ -144,30 +144,29 @@ const CreateQuiz = (props) => {
       } else {
         alert("Minimum Questions Reached!")
       }
-    }
   };
 
-    const questionValidation = (callback) => {
-      if (TFInputFields.length === 0) {
-        for (var i = 0; i < MCInputFields.length; i++) {
-          if (MCInputFields[i].question.length < 1) {
-            setMCValidation(false);
-          }
-          if (MCInputFields[i].corrAns.length < 1) {
-            setMCValidation(false);
-          }
-          if (MCInputFields[i].incAns1.length < 1) {
-            setMCValidation(false);
-          }
-          if (MCInputFields[i].incAns2.length < 1) {
-            setMCValidation(false);
-          }
-          if (MCInputFields[i].incAns3.length < 1) {
-            setMCValidation(false);
-          }
+  const questionValidation = (callback) => {
+    if (TFInputFields.length === 0) {
+      for (var i = 0; i < MCInputFields.length; i++) {
+        if (MCInputFields[i].question.length < 1) {
+          setMCValidation(false);
         }
-        callback(true, null)
-      } else if (MCInputFields.length === 0) {
+        if (MCInputFields[i].corrAns.length < 1) {
+          setMCValidation(false);
+        }
+        if (MCInputFields[i].incAns1.length < 1) {
+          setMCValidation(false);
+        }
+        if (MCInputFields[i].incAns2.length < 1) {
+          setMCValidation(false);
+        }
+        if (MCInputFields[i].incAns3.length < 1) {
+          setMCValidation(false);
+        }
+      }
+      callback(true, null)
+    } else if (MCInputFields.length === 0) {
         for (var j = 0; j < TFInputFields.length; j++) {
           if (TFInputFields[j].question.length < 1) {
             setTFValidation(false);
@@ -181,40 +180,39 @@ const CreateQuiz = (props) => {
         }
         callback(null, true)
 
-      } else {
-          for (var k = 0; k < MCInputFields.length; k++) {
-            if (MCInputFields[k].question.length < 1) {
-              setMCValidation(false);
-            }
-            if (MCInputFields[k].corrAns.length < 1) {
-              setMCValidation(false);
-            }
-            if (MCInputFields[k].incAns1.length < 1) {
-              setMCValidation(false);
-            }
-            if (MCInputFields[k].incAns2.length < 1) {
-              setMCValidation(false);
-            }
-            if (MCInputFields[k].incAns3.length < 1) {
-              setMCValidation(false);
-            }
+    } else {
+        for (var k = 0; k < MCInputFields.length; k++) {
+          if (MCInputFields[k].question.length < 1) {
+            setMCValidation(false);
           }
+          if (MCInputFields[k].corrAns.length < 1) {
+            setMCValidation(false);
+          }
+          if (MCInputFields[k].incAns1.length < 1) {
+            setMCValidation(false);
+          }
+          if (MCInputFields[k].incAns2.length < 1) {
+            setMCValidation(false);
+          }
+          if (MCInputFields[k].incAns3.length < 1) {
+            setMCValidation(false);
+          }
+        }
 
-          for (var l = 0; l < TFInputFields.length; l++) {
-            if (TFInputFields[l].question.length < 1) {
-              setTFValidation(false);
-            }
-            if (TFInputFields[l].corrAns.length < 1) {
-              setTFValidation(false);
-            }
-            if (TFInputFields[l].incAns.length < 1) {
-              setTFValidation(false);
-            }
+        for (var l = 0; l < TFInputFields.length; l++) {
+          if (TFInputFields[l].question.length < 1) {
+            setTFValidation(false);
           }
+          if (TFInputFields[l].corrAns.length < 1) {
+            setTFValidation(false);
+          }
+          if (TFInputFields[l].incAns.length < 1) {
+            setTFValidation(false);
+          }
+        }
         callback(null, null);
       }
     }
-  };
 
     const sendQuiz = (e) => {
 
@@ -226,18 +224,17 @@ const CreateQuiz = (props) => {
         } else if (!MCValidation && MC === true) {
           alert("Please Fill Out All MC Question Fields!");
         } else if (!MCValidation && TFValidation && !MC && !TF) {
-          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions")
+          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions");
         } else if (MCValidation && !TFValidation && !MC && !TF) {
-          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions")
+          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions");
         } else if (!MCValidation && !TFValidation && !MC && !TF) {
-          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions")
+          alert("Please Fill Out All MC / TF Question Fields or Remove Unused Questions");
         } else if (!quizName) {
-          alert("Please Enter Quiz Name!')
+          alert("Please Enter Quiz Name!")
         } else if (!categoryVal) {
-          alert("Please Choose Category!')
+          alert("Please Choose Category!")
         } else {
           if (TF) {
-
             var quizDataTF = {
               quizzes: {
               user_id: "admin",
@@ -262,7 +259,6 @@ const CreateQuiz = (props) => {
                 window.location.href = "http:localhost:3000/";
               }
             })
-
           } else if (MC) {
             var quizDataMC = {
               quizzes: {
@@ -315,55 +311,54 @@ const CreateQuiz = (props) => {
             })
           }
         }
-      }
-    });
-  };
+      })
+    }
 
-  return (
-    <div className="createQuiz">
-      <div name="quizName">
-        <h1>Name Your Quiz!</h1>
-        <input
-          value={quizName}
-          placeholder={"Enter Quiz Name Here"}
-          onChange={nameChange}
-        ></input>
+    return (
+      <div className="createQuiz">
+        <div name="quizName">
+          <h1>Name Your Quiz!</h1>
+          <input
+            value={quizName}
+            placeholder={"Enter Quiz Name Here"}
+            onChange={nameChange}
+          ></input>
+        </div>
+        <Categories
+          category1={category1}
+          category2={category2}
+          category3={category3}
+          category4={category4}
+          category5={category5}
+          select={onSelect}
+        />
+        <MCQuestions
+          inputFields={MCInputFields}
+          setInputFields={setMCInputFields}
+          handleFormChange={handleFormChange}
+          addFields={addFields}
+          removeFields={removeFields}
+        />
+        <TFQuestions
+          inputFields={TFInputFields}
+          setInputFields={setTFInputFields}
+          handleFormChange={handleFormChange}
+          addFields={addFields}
+          removeFields={removeFields}
+        />
+        <APIQuestions />
+        <div>
+          <button
+            onClick={(e) => {
+              setTimeout(sendQuiz(e), 5000);
+            }}
+          >
+            {" "}
+            Create Quiz!{" "}
+          </button>
+        </div>
       </div>
-      <Categories
-        category1={category1}
-        category2={category2}
-        category3={category3}
-        category4={category4}
-        category5={category5}
-        select={onSelect}
-      />
-      <MCQuestions
-        inputFields={MCInputFields}
-        setInputFields={setMCInputFields}
-        handleFormChange={handleFormChange}
-        addFields={addFields}
-        removeFields={removeFields}
-      />
-      <TFQuestions
-        inputFields={TFInputFields}
-        setInputFields={setTFInputFields}
-        handleFormChange={handleFormChange}
-        addFields={addFields}
-        removeFields={removeFields}
-      />
-      <APIQuestions />
-      <div>
-        <button
-          onClick={(e) => {
-            setTimeout(sendQuiz(e), 5000);
-          }}
-        >
-          {" "}
-          Create Quiz!{" "}
-        </button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
 
 export default CreateQuiz;
