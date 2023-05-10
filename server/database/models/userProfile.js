aec3526d08031115999b7b2dd24af7d4d98704bd
Patty Long <pattygxlong@gmail.com>
2023-05-09 21:53:53 -0700

    Basic skeleton for db methods

---
+++ server/database/models/userProfile.js
@@ -0,0 +1,24 @@
+const db = require('../../index.js');
+
+module.exports = {
+
+  getUserInfo: (/* TBD */) => {
+    // query logic
+  },
+
+  updateUsername: (/* TBD */) => {
+    // query logic
+  },
+
+  updatePassword: (/* TBD */) => {
+    // query logic
+  },
+
+  updateBio: (/* TBD */) => {
+    // query logic
+  },
+
+  updateProfilePic: (/* TBD */) => {
+    // query logic
+  },
+};
