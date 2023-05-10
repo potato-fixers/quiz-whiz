d3a932d1e228c78521fca3b7144136b8d2682c11
Patty Long <pattygxlong@gmail.com>
2023-05-09 20:36:45 -0700

    Add routes to each component to save each field

--- client/src/components/user-profile/UserProfilePage.jsx
+++ client/src/components/user-profile/UserProfilePage.jsx
@@ -5,10 +5,10 @@ import UserProfileFieldBox from './UserProfileFieldBox.jsx';
 function UserProfilePage() {
   return (
     <div>
-      <UserProfileFieldBox field_title={"Profile Picture"} label={"Profile Picture"} initial_value={"pic"} />
+      <UserProfileFieldBox field_title={"Profile Picture"} label={"Profile Picture"} initial_value={"pic"} saveRoute={'/settings/updatePic'}/>
       <UserProfileFieldBox field_title={"Username"} label={"Username"} initial_value={"johndoe"} saveRoute={'/settings/updateUsername'}/>
-      <UserProfileFieldBox field_title={"Password"} label={"Password"} initial_value={"password"} />
-      <UserProfileFieldBox field_title={"Bio"} label={"bio"} initial_value={"Roses are red, violets are blue, I like taking quizzes, how bout you!"} />
+      <UserProfileFieldBox field_title={"Password"} label={"Password"} initial_value={"password"} saveRoute={'/settings/updatePassword'} />
+      <UserProfileFieldBox field_title={"Bio"} label={"bio"} initial_value={"Roses are red, violets are blue, I like taking quizzes, how bout you!"} saveRoute={'/settings/updateBio'}/>
     </div>
   );
 }
