32517571549faffdc8f41275973893f09acc06bb
Thanh Ly <thanhgly.se@gmail.com>
2023-05-09 17:27:00 -0700

    implemented a useFilter hook to handle filtering quiz and tested it on MyQuizzes

--- client/src/components/user-dashboard/components/subComponents/FilterBar.jsx
+++ client/src/components/user-dashboard/components/subComponents/FilterBar.jsx
@@ -1,34 +1,25 @@
 import { Stack, OutlinedInput, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
-import { useState } from 'react';
 
-const FilterBar = () => {
-
-  const [term, setTerm] = useState('');
-  const [category, setCategory] = useState('');
+const FilterBar = (props) => {
 
   const handleChange = (e) => {
-    let value = e.target.value;
-    setTerm(value);
-  };
-
-  const handleSelect = (e) => {
-    let value = e.target.value;
-    setCategory(value);
+    const { name, value } = e.target;
+    props.onFilterChange(name, value.toLowerCase());
   };
 
   return (
     <Stack direction='row' spacing={1} justifyContent='flex-end'>
-      <OutlinedInput size='small' placeholder='Search quiz' value={term} onChange={handleChange}/>
+      <OutlinedInput size='small' placeholder='Search quiz' name='name' onChange={handleChange}/>
       <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
         <InputLabel id="select-label">Categories</InputLabel>
         <Select
           labelId='select-label'
           label='Categories'
-          value={category}
-          onChange={handleSelect}
+          name='category'
+          onChange={handleChange}
           size='small'
         >
-          <MenuItem value='all'>All</MenuItem>
+          <MenuItem value=''>All</MenuItem>
           <MenuItem value={'education'}>Education</MenuItem>
           <MenuItem value={'music'}>Music</MenuItem>
           <MenuItem value={'sport'}>Sport</MenuItem>
---
+++ client/src/components/user-dashboard/hooks/useFilter.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useFilter = (initialData) => {
+
+  const [filter, setFilter] = useState({name: '', category: ''});
+
+  const handleFilterChange = (name, value) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      [name]: value
+    }));
+  };
+
+  const filteredData = initialData.filter(quiz =>
+    quiz.quiz_name.toLowerCase().includes(filter.name) &&
+    quiz.category.toLowerCase().includes(filter.category)
+  );
+
+  return {
+    filteredData,
+    handleFilterChange
+  }
+};
+
+export default useFilter;
