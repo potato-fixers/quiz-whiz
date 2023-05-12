ac13a00350b18bcdbc1ce2342e147c545fff6351
Patty Long <pattygxlong@gmail.com>
2023-05-11 18:54:34 -0700

    Updating index with cors brackets

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ const cors = require('cors');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(cors(
+app.use(cors({
   origin: 'http://localhost:3000'
-))
+}));
 
 // =============================================
 //               Route Imports
