beeafaab222f0c9ce7ce5657b143afa44a6f1791
bennyv8 <bvanhr@gmail.com>
2023-05-12 23:19:56 -0700

    moved userProvider to pass App.test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,31 +7,35 @@ import Dashboard from "./components/user-dashboard/Dashboard.jsx";
 import Login from "../src/pages/Login";
 import Register from "../src/pages/Register";
 import UserProfilePage from './components/user-profile/UserProfilePage.jsx';
-import CreateQuiz from './components/create-quiz/CreateQuiz.jsx'
+import CreateQuiz from './components/create-quiz/CreateQuiz.jsx';
+import { UserProvider } from './components/global/UserContext.jsx';
+
 
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Routes>
-        <Route
-          path="/*"
-          element={
-            <div className="body">
-              <Typography variant="h4">Welcome to Quiz Whiz</Typography>
-            </div>
-          }
-        ></Route>
+    <UserProvider>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            path="/*"
+            element={
+              <div className="body">
+                <Typography variant="h4">Welcome to Quiz Whiz</Typography>
+              </div>
+            }
+          ></Route>
 
-        <Route path="/quiz/:id/*" element={<TakeQuiz />}></Route>
-        <Route path="dashboard/*" element={<Dashboard />}></Route>
+          <Route path="/quiz/:id/*" element={<TakeQuiz />}></Route>
+          <Route path="dashboard/*" element={<Dashboard />}></Route>
 
-        <Route path="/login/" element={<Login />}></Route>
-        <Route path="/register/" element={<Register />}></Route>
-        <Route path="/settings/" element={<UserProfilePage/>}></Route>
-        <Route path='/createQuiz/' element={<CreateQuiz />}></Route>
-      </Routes>
-    </Router>
+          <Route path="/login/" element={<Login />}></Route>
+          <Route path="/register/" element={<Register />}></Route>
+          <Route path="/settings/" element={<UserProfilePage />}></Route>
+          <Route path='/createQuiz/' element={<CreateQuiz />}></Route>
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
 
