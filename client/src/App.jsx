8f772c71c2b9ece34b3475fe80123b4fecdd35fa
Gabriela Taylor <gabriela@Gabrielas-iMac.lan>
2023-05-13 12:24:45 -0400

    Resolve App.jsx conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import "./styles/App.css";
 import { Route, Routes } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
 
 import Nav from "./Nav.jsx";
-import TakeQuiz from "./components/take-quiz/TakeQuiz.jsx";
-import Dashboard from "./components/user-dashboard/Dashboard.jsx";
+import Landing from "./components/home-landing/Landing.jsx";
 import Login from "../src/pages/Login";
 import Register from "../src/pages/Register";
 import UserProfilePage from "./components/user-profile/UserProfilePage.jsx";
+import Dashboard from "./components/user-dashboard/Dashboard.jsx";
+import TakeQuiz from "./components/take-quiz/TakeQuiz.jsx";
 import CreateQuiz from "./components/create-quiz/CreateQuiz.jsx";
+
 import { QuizProvider } from "./components/take-quiz/context/QuizContext";
 function App() {
   return (
@@ -16,14 +17,6 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/*" element={<Landing />}></Route>
-        <Route
-          path="/*"
-          element={
-            <div className="body">
-              <Typography variant="h4">Welcome to Quiz Whiz</Typography>
-            </div>
-          }
-        ></Route>
 
         <Route
           path="/quiz/:id/*"
