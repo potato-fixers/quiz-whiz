38c7bdaec7cf4b01a0eac48367710a0b9bed3d94
Thanh Ly <thanhgly.se@gmail.com>
2023-05-14 01:28:06 -0700

    code coverage of user dashboard is over 70%

--- client/src/components/user-dashboard/__test__/Dashboard.test.js
+++ client/src/components/user-dashboard/__test__/Dashboard.test.js
@@ -54,4 +54,117 @@ test('changes active tab on tab click', async () => {
   await waitFor(() =>  expect(screen.getByRole('tab', {selected: true})).toHaveTextContent('Favorites'));
 });
 
+test('overview tab', async () => {
+  render(
+    <UserProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    </UserProvider>
+  );
 
+   // Assert that the Overview component is rendered
+   expect(screen.getByText('Recent plays')).toBeInTheDocument();
+
+   // Assert that the quiz data is displayed correctly
+   expect(await screen.findByText('Quiz E')).toBeInTheDocument();
+   expect(screen.getByText('73%')).toBeInTheDocument();
+   expect(screen.getByText('Quiz D')).toBeInTheDocument();
+   expect(screen.getByText('90%')).toBeInTheDocument();
+});
+
+test('my quizzes tab', async () => {
+  render(
+    <UserProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    </UserProvider>
+  );
+
+  // Simulate clicking on the 'my quizzes' tab
+  fireEvent.click(screen.getByText(/My Quizzes/i));
+
+  // Assert that the My Quizzes component is rendered
+  expect(screen.getAllByText('My Quizzes').length).toBe(2);
+
+  // Assert that the quiz data is displayed correctly
+  expect(await screen.findByText('Math Quiz')).toBeInTheDocument();
+  expect(await screen.findByText('Song Quiz')).toBeInTheDocument();
+  expect(await screen.findByText('Bollywood movie quiz')).toBeInTheDocument();
+  expect(await screen.findByText('Lakers or Warriors?')).toBeInTheDocument();
+  expect(await screen.findByText('More math quiz')).toBeInTheDocument();
+
+  // Simulate clicking on quiz name
+  fireEvent.click(screen.getByText(/Song Quiz/i));
+
+  // Assert that the the page was redirect to taking quiz
+  expect(await screen.findByText(/CATEGORY/i)).toBeInTheDocument();
+
+  // Simulate clicking headers
+  fireEvent.click(screen.getByText(/Likes/i));
+});
+
+test('history tab', async () => {
+  render(
+    <UserProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    </UserProvider>
+  );
+
+  // Simulate clicking on the 'my quizzes' tab
+  fireEvent.click(screen.getByText(/History/i));
+
+  // Assert that the My Quizzes component is rendered
+  expect(screen.getAllByText('History').length).toBe(2);
+
+  // Assert that the quiz data is displayed correctly
+  expect(await screen.findByText('Quiz E')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz D')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz C')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz B')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz A')).toBeInTheDocument();
+
+  // Simulate clicking on quiz name
+  fireEvent.click(screen.getByText(/Quiz A/i));
+
+  // Assert that the the page was redirect to taking quiz
+  expect(await screen.findByText(/CATEGORY/i)).toBeInTheDocument();
+
+  // Simulate clicking headers
+  fireEvent.click(screen.getByText(/Best Score/i));
+});
+
+test('favorites tab', async () => {
+  render(
+    <UserProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
+    </UserProvider>
+  );
+
+  // Simulate clicking on the 'my quizzes' tab
+  fireEvent.click(screen.getAllByText(/Favorites/i)[0]);
+
+  // Assert that the My Quizzes component is rendered
+  expect(screen.getAllByText('Favorites').length).toBe(3);
+
+  // Assert that the quiz data is displayed correctly
+  expect(await screen.findByText('Quiz E')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz D')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz C')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz B')).toBeInTheDocument();
+  expect(await screen.findByText('Quiz A')).toBeInTheDocument();
+
+  // Simulate clicking on quiz name
+  fireEvent.click(screen.getByText(/Quiz A/i));
+
+  // Assert that the the page was redirect to taking quiz
+  expect(await screen.findByText(/CATEGORY/i)).toBeInTheDocument();
+
+  // Simulate clicking headers
+  fireEvent.click(screen.getByText(/Total Likes/i));
+});
