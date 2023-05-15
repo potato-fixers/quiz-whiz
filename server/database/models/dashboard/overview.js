d1bb647af9d94777166aa77405888342ea14e8b6
Thanh Ly <thanhgly.se@gmail.com>
2023-05-15 14:14:59 -0700

    wrote up a queryString for overview

--- server/database/models/dashboard/overview.js
+++ server/database/models/dashboard/overview.js
@@ -36,9 +36,31 @@ const data = [
 
 module.exports = {
 
-  get: (/* TBD */) => {
-    // query logic
-    return data;
+  get: (userId) => {
+
+    const queryString = `
+      SELECT
+        quizzes.id,
+        quizzes.quiz_name,
+        history.score,
+        to_char(history.date, 'FMMonth FMDDth, YYYY') AS date
+      FROM
+        quizzes
+        INNER JOIN history ON quizzes.id = history.quiz_id
+      WHERE
+        quizzes.user_id = ${userId}
+      ORDER BY
+        history.date DESC
+      LIMIT 10
+    `;
+
+    return db.query(queryString)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      console.error(err.stack);
+    });
   },
 
 };
