1a4dc1652a762e598902dd31a5563c8b69245283
Thanh Ly <thanhgly.se@gmail.com>
2023-05-15 14:08:00 -0700

    Rollbacked useQuizzes.jsx

--- client/src/components/user-dashboard/hooks/useQuizzes.jsx
+++ client/src/components/user-dashboard/hooks/useQuizzes.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 const useQuizzes = (tab) => {
   const [quizzes, setQuizzes] = useState([]);
 
   const getQuizzes = (userId) => {
     const url = process.env.REACT_APP_API_URI;
-    axios.get(`${url}/dashboard/${tab}`)
+    fetch(`${url}/dashboard/${tab}`)
     .then( async (res) => {
-      setQuizzes(res.data);
+      setQuizzes(await res.json());
     })
     .catch(err => {
       console.error(err.stack);
