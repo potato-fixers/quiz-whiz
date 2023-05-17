16c0fdcc09227a8938889db3a70ed9a3f6124daf
Gabriela Taylor <gabriela@Gabrielas-iMac.lan>
2023-05-17 19:05:09 -0400

    Added clicked button styling for Quiz Question

--- client/src/components/take-quiz/quiz-components/Answers.jsx
+++ client/src/components/take-quiz/quiz-components/Answers.jsx
@@ -7,12 +7,18 @@ function Answers({ page, answers }) {
 
   const handleClick = (e) => {
     let selected = Object.values(e.currentTarget.firstChild)[0].stateNode.data;
+
+    document.querySelectorAll('.btn').forEach(item => {
+      item.setAttribute('style', 'null')
+    })
+
     keys.forEach((key, index) => {
       if (answers[index] === selected) {
         selected = { key: key, value: selected, question: page };
       }
     });
     localStorage.setItem(`${page}`, JSON.stringify(selected));
+    e.currentTarget.setAttribute('style','box-shadow: inset 3px 3px 3px 3px var(--dk-pink);')
   };
 
   return (
@@ -23,8 +29,10 @@ function Answers({ page, answers }) {
             return (
               <Button
                 key={index}
+                className='btn'
                 variant="contained"
                 color="secondary"
+                style={{}}
                 onClick={handleClick}
               >
                 {answers[index]}
