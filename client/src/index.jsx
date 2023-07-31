16b645409e6a83f95a9a835c4440ba80ff8d5e70
bennyv8 <bvanhr@gmail.com>
2023-07-31 14:43:24 -0700

    renamed index.js to index.jsx for vite

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ import { ThemeProvider, createTheme } from '@mui/material';
 import { UserProvider } from './components/global/UserContext.jsx';
 
 import App from './App';
+
 // import reportWebVitals from './reportWebVitals';
 
 const theme = createTheme({
@@ -40,4 +41,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();  
+// reportWebVitals();
