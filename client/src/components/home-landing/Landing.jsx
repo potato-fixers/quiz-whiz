bb72fb31581e0976ffc6f34f900c2199698e64d4
bennyv8 <bvanhr@gmail.com>
2023-08-06 11:29:56 -0700

    moved title to center in landing pange

--- client/src/components/home-landing/Landing.jsx
+++ client/src/components/home-landing/Landing.jsx
@@ -61,7 +61,7 @@ const Landing = (props) => {
   if (profile.userId === undefined) {
     return (
       <Container className="Landing" align="center">
-        <Typography variant="h2" align="left" sx={{fontWeight: 400, marginBottom: 2.5}}> Welcome to Quiz Whiz </Typography>
+        <Typography variant="h2" align="center" sx={{fontWeight: 400, marginBottom: 2.5, marginTop: 2.5}}> Welcome to Quiz Whiz </Typography>
         <CategoryList setCategory={setCategory} category={category}/>
         <Typography variant="h6" align="center" sx={{fontWeight: 400, marginBottom: 2.5}}> Login to see all Quizzes </Typography>
         <QuizList category={category} quizzes={quizzes}/>
